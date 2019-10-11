d688c85aba78c23086f58c3574c2bc7a9d545451
Joshua Cork <joshua.cork@gmail.com>
2019-10-11 14:58:51 -0700

    stuff

--- eth-contracts/contracts/SolnSquareVerifier.sol
+++ eth-contracts/contracts/SolnSquareVerifier.sol
@@ -10,7 +10,6 @@ import "@openzeppelin/contracts/drafts/Counters.sol";
 // DONE define another contract named SolnSquareVerifier that inherits from your ERC721Mintable class
 contract SolnSquareVerifier is AwesomeTitle, Verifier {
     using Counters for Counters.Counter;
-
     Counters.Counter private _tokenIds;
     string internal constant _baseTokenURI = "https://s3-us-west-2.amazonaws.com/udacity-blockchain/capstone/";
 
@@ -29,15 +28,24 @@ contract SolnSquareVerifier is AwesomeTitle, Verifier {
     // DONE: Create an event to emit when a solution is added
     event SolutionSubmitted(address submittedBy);
 
+    event HashCreated(bytes32 theHash);
+    event AddSolutionCalled(string theString);
+    event TestingYo(string aString);
+
+    function testFunction() external {
+        emit TestingYo("THIS IS A TEST YO!");
+        awesomeTitleTest();
+    }
+
     function solutionExists(bytes32 theHash, address theAddress)
     internal
     returns (bool)
     {
-        if (submittedSolutions[theAddress].solutions.length == 0) {
+        if (submittedSolutions[theAddress].length == 0) {
             return false;
         } else { // address has submitted solutions, need to iterate over them.
-            for(uint i = 0; i < submittedSolutions[theAddress].solutions.length; i++) {
-                if(submittedSolutions[theAddress].solutions[i].solutionHash == theHash){
+            for(uint i = 0; i < submittedSolutions[theAddress].length; i++) {
+                if(submittedSolutions[theAddress][i].solutionHash == theHash){
                     return true; // solution has already been submitted by this address.
                 }
                 return false; // this is a new solution.
@@ -47,17 +55,20 @@ contract SolnSquareVerifier is AwesomeTitle, Verifier {
     }
 
     // DONE Create a function to add the solutions to the array and emit the event
-    function addSolution(uint[2] pa, uint[2][2] pb, uint[2] pc, uint[2] i, address to)
+    function addSolution(uint[2] calldata pa, uint[2][2] calldata pb, uint[2] calldata pc, uint[2] calldata i, address to)
     external
     {
+        emit AddSolutionCalled("I have been called");
         bytes32 newHash = keccak256(abi.encodePacked(pa, pb, pc, i));
+        emit HashCreated(newHash);
         require(solutionExists(newHash, to) == false, "Solution has already been submitted by this address");
+
 
         // Solution doesn't already exist, add to solutions for this address
         Solution memory newSolution;
         newSolution.solutionHash = newHash;
         newSolution.someAddress = to;
-        submittedSolutions[to].solutions.push(newSolution);
+        submittedSolutions[to].push(newSolution);
         emit SolutionSubmitted(msg.sender);
 
         bool solutionIsValid = verifyTx(pa,pb,pc,i);
@@ -89,12 +100,3 @@ contract SolnSquareVerifier is AwesomeTitle, Verifier {
     return success;
     }
 }
-
-
-
-
-
-
-
-
-
